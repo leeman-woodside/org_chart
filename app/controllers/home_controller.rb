1d17562011f05c406d4a68dcbcf46249e5fb8436
Lee Woodside <lee.woodside@podium.com>
2023-11-19 22:53:28 -0800

    some ground work laid down, now to process the data

---
+++ app/controllers/home_controller.rb
@@ -0,0 +1,15 @@
+class HomeController < ApplicationController
+  def index
+  end
+
+  def import_csv
+    uploaded_file = params[:csv_file]
+
+    if uploaded_file.present?
+      Ingest.process_csv(uploaded_file)
+      redirect_to root_path, notice: 'CSV file imported successfully'
+    else
+      redirect_to root_path, alert: 'No CSV file uploaded'
+    end
+  end
+end
--- app/services/ingest.rb
+++ app/services/ingest.rb
@@ -1,4 +1,12 @@
 class Ingest
-  def run(args)
+  def self.process_csv(file)
+    csv_data = CSV.read(file.path, headers: true)
+
+    # Process the CSV data and create database records
+    csv_data.each do |row|
+      # Process each row and create database records
+    end
+
+    # Return any relevant information about the import process
   end
 end
