1b8a55d50f6b7460083bb71601439e690164ea75
Lee Woodside <lee.woodside@podium.com>
2023-11-20 22:09:12 -0800

    not quite there with the squad ids...

--- app/services/ingest.rb
+++ app/services/ingest.rb
@@ -4,17 +4,7 @@ require 'csv'
 class Ingest
   def self.process_csv(file)
     CSV.foreach(file, headers: true) do |row|
-      # CSV has columns named:
-      # Contributor email 
-      # Squad name 
-      # Squad DRI email
-      # Group name 
-      # Group DRI email 
-      # Chapter name 
-      # Chapter DRI email 
-      # Department name 
-      # Department DRI email
-      # Create or find the users
+
       dri_email = row['Squad DRI email']
       contributor_email = row['Contributor email']
 
@@ -24,6 +14,15 @@ class Ingest
       # Assign squad_id to users if available
       squad_name = row['Squad name']
       squad = Squad.find_or_create_by(name: squad_name)
+
+      chapter_name = row['Chapter name']
+      group_name = row['Group name']
+      department_name = row['Department name']
+
+      # Create or find rest of the squads
+      Squad.find_or_create_by(name: chapter_name)
+      Squad.find_or_create_by(name: group_name)
+      Squad.find_or_create_by(name: department_name)
 
       dri_user.update(squad_id: squad.id) if dri_user.squad_id.nil?
       contributor_user.update(squad_id: squad.id) if contributor_user.squad_id.nil?
